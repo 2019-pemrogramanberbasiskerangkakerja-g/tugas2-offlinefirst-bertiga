e6a0d00df3c9334f2b617a219fa0acf8f3ba1315
darke-f <ariiq.pooka@gmail.com>
2019-03-03 19:24:32 +0700

    Session, log, replicate

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var express = require('express'),
     nano = require('nano')('http://localhost:5984'),
-    users = nano.db.use('users'),
+    nanoweb = require('nano')('https://couchdb-2ac681.smileupps.com/'),
+    users = nanoweb.db.use('users'),
+    log = nano.db.use('log'),
     qs = require('querystring'),
     bcrypt = require('bcrypt-nodejs'),
+    cookieParser = require('cookie-parser'),
     session = require('express-session'),
     data = []
 
 var app = express()
+
+app.use(cookieParser());
+app.use(session({secret: "Shh, its a secret!"}));
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
 app.get('/login', function(req, res){
-    res.sendFile(__dirname + '/view/login.html')
+    if(req.session.username) {
+        res.send('logged in')
+    }
+    else res.sendFile(__dirname + '/view/login.html')
 })
 
 app.post('/login', function(req, res){
@@ -31,9 +41,17 @@ app.post('/login', function(req, res){
                 // }
                 // app.set('truct proxy', 1)
                 // sess.cookie.secure = true
+                req.session.username = data.username
+                console.log(req.session.username)
+                var today = new Date();
+                var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                var dateTime = date+' '+time;
+                log.insert({ _id: dateTime, log: "login suksess" })
                 res.send('logged in')
             }
             else{
+                log.insert({ _id: dateTime, log: "login gagal (password salah)" })
                 res.send('password wrong')
             }
         })
